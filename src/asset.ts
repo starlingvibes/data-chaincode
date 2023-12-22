1bb0a566d0bb0a0f44518cc2249b20bdc6b4c389
starlingvibes <starlingroot@protonmail.com>
2023-12-22 15:07:46 +0100

    fix(): fixed issue with chaincode

--- src/asset.ts
+++ src/asset.ts
@@ -10,41 +10,8 @@ export class Asset {
   public docType?: string;
 
   @Property()
-  public ID: string;
+  public Timestamp: string;
 
   @Property()
-  public SNR: number;
-
-  @Property()
-  public VBAT: number;
-
-  @Property()
-  public Latitude: number;
-
-  @Property()
-  public Longitude: number;
-
-  @Property()
-  public Gas_resistance: number;
-
-  @Property()
-  public Temperature: number;
-
-  @Property()
-  public Pressure: number;
-
-  @Property()
-  public Humidity: number;
-
-  @Property()
-  public Light: number;
-
-  @Property()
-  public Gyroscope: number[];
-
-  @Property()
-  public Accelerometer: number[];
-
-  @Property()
-  public Owner: string;
+  public Methanelevel: number;
 }
--- src/assetTransfer.ts
+++ src/assetTransfer.ts
@@ -15,26 +15,15 @@ import { Asset } from './asset';
 
 @Info({
   title: 'AssetTransfer',
-  description: 'Smart contract for storing and trading data assets',
+  description: 'Smart contract for storing and fetching methane level data',
 })
 export class AssetTransferContract extends Contract {
   @Transaction()
   public async InitLedger(ctx: Context): Promise<void> {
     const assets: Asset[] = [
       {
-        ID: 'basdni7s8acadad8a9d8',
-        SNR: 0.5,
-        VBAT: 3.3,
-        Latitude: 45.464664,
-        Longitude: 12.2629,
-        Gas_resistance: 0.5,
-        Temperature: 31.7,
-        Pressure: 1000.5,
-        Humidity: 50.5,
-        Light: 0.5,
-        Gyroscope: [0.5, 0.5, 0.5],
-        Accelerometer: [0.5, 0.5, 0.5],
-        Owner: `Chidera's IoT device 1`,
+        Timestamp: '00:00:00',
+        Methanelevel: 0,
       },
     ];
 
@@ -45,10 +34,10 @@ export class AssetTransferContract extends Contract {
       // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
       // when retrieving data, in any lang, the order of data will be the same and consequently also the corresonding hash
       await ctx.stub.putState(
-        asset.ID,
+        asset.Timestamp,
         Buffer.from(stringify(sortKeysRecursive(asset)))
       );
-      console.info(`Asset ${asset.ID} initialized`);
+      console.info(`Asset ${asset.Timestamp} initialized`);
     }
   }
 
@@ -56,53 +45,31 @@ export class AssetTransferContract extends Contract {
   @Transaction()
   public async CreateAsset(
     ctx: Context,
-    id: string,
-    snr: number,
-    vbat: number,
-    latitude: number,
-    longitude: number,
-    gas_resistance: number,
-    temperature: number,
-    pressure: number,
-    humidity: number,
-    light: number,
-    gyroscope: number[],
-    accelerometer: number[],
-    owner: string
+    timestamp: string,
+    methanelevel: number
   ): Promise<void> {
-    const exists = await this.AssetExists(ctx, id);
+    const exists = await this.AssetExists(ctx, timestamp);
     if (exists) {
-      throw new Error(`The asset ${id} already exists`);
+      throw new Error(`Data for timestamp ${timestamp} already exists`);
     }
 
     const asset = {
-      ID: id,
-      SNR: snr,
-      VBAT: vbat,
-      Latitude: latitude,
-      Longitude: longitude,
-      Gas_resistance: gas_resistance,
-      Temperature: temperature,
-      Pressure: pressure,
-      Humidity: humidity,
-      Light: light,
-      Gyroscope: gyroscope,
-      Accelerometer: accelerometer,
-      Owner: owner,
+      Timestamp: timestamp,
+      Methanelevel: methanelevel,
     };
     // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
     await ctx.stub.putState(
-      id,
+      timestamp,
       Buffer.from(stringify(sortKeysRecursive(asset)))
     );
   }
 
   // ReadAsset returns the asset stored in the world state with given id.
   @Transaction(false)
-  public async ReadAsset(ctx: Context, id: string): Promise<string> {
-    const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
+  public async ReadAsset(ctx: Context, timestamp: string): Promise<string> {
+    const assetJSON = await ctx.stub.getState(timestamp); // get the asset from chaincode state
     if (!assetJSON || assetJSON.length === 0) {
-      throw new Error(`The asset ${id} does not exist`);
+      throw new Error(`Data for timestamp ${timestamp} does not exist`);
     }
     return assetJSON.toString();
   }
@@ -111,84 +78,62 @@ export class AssetTransferContract extends Contract {
   @Transaction()
   public async UpdateAsset(
     ctx: Context,
-    id: string,
-    snr: number,
-    vbat: number,
-    latitude: number,
-    longitude: number,
-    gas_resistance: number,
-    temperature: number,
-    pressure: number,
-    humidity: number,
-    light: number,
-    gyroscope: number[],
-    accelerometer: number[],
-    owner: string
+    timestamp: string,
+    methanelevel: number
   ): Promise<void> {
-    const exists = await this.AssetExists(ctx, id);
+    const exists = await this.AssetExists(ctx, timestamp);
     if (!exists) {
-      throw new Error(`The asset ${id} does not exist`);
+      throw new Error(`Data for timestamp ${timestamp} does not exist`);
     }
 
     // overwriting original asset with new asset
     const updatedAsset = {
-      ID: id,
-      SNR: snr,
-      VBAT: vbat,
-      Latitude: latitude,
-      Longitude: longitude,
-      Gas_resistance: gas_resistance,
-      Temperature: temperature,
-      Pressure: pressure,
-      Humidity: humidity,
-      Light: light,
-      Gyroscope: gyroscope,
-      Accelerometer: accelerometer,
-      Owner: owner,
+      Timestamp: timestamp,
+      Methanelevel: methanelevel,
     };
     // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
     return ctx.stub.putState(
-      id,
+      timestamp,
       Buffer.from(stringify(sortKeysRecursive(updatedAsset)))
     );
   }
 
   // DeleteAsset deletes an given asset from the world state.
   @Transaction()
-  public async DeleteAsset(ctx: Context, id: string): Promise<void> {
+  public async DeleteAsset(ctx: Context, timestamp: string): Promise<void> {
     const exists = await this.AssetExists(ctx, id);
     if (!exists) {
-      throw new Error(`The asset ${id} does not exist`);
+      throw new Error(`Data for timestamp ${timestamp} does not exist`);
     }
-    return ctx.stub.deleteState(id);
+    return ctx.stub.deleteState(timestamp);
   }
 
   // AssetExists returns true when asset with given ID exists in world state.
   @Transaction(false)
   @Returns('boolean')
-  public async AssetExists(ctx: Context, id: string): Promise<boolean> {
-    const assetJSON = await ctx.stub.getState(id);
+  public async AssetExists(ctx: Context, timestamp: string): Promise<boolean> {
+    const assetJSON = await ctx.stub.getState(timestamp);
     return assetJSON && assetJSON.length > 0;
   }
 
   // TransferAsset updates the owner field of asset with given id in the world state, and returns the old owner.
-  @Transaction()
-  public async TransferAsset(
-    ctx: Context,
-    id: string,
-    newOwner: string
-  ): Promise<string> {
-    const assetString = await this.ReadAsset(ctx, id);
-    const asset = JSON.parse(assetString);
-    const oldOwner = asset.Owner;
-    asset.Owner = newOwner;
-    // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-    await ctx.stub.putState(
-      id,
-      Buffer.from(stringify(sortKeysRecursive(asset)))
-    );
-    return oldOwner;
-  }
+  // @Transaction()
+  // public async TransferAsset(
+  //   ctx: Context,
+  //   id: string,
+  //   newOwner: string
+  // ): Promise<string> {
+  //   const assetString = await this.ReadAsset(ctx, id);
+  //   const asset = JSON.parse(assetString);
+  //   const oldOwner = asset.Owner;
+  //   asset.Owner = newOwner;
+  //   // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
+  //   await ctx.stub.putState(
+  //     id,
+  //     Buffer.from(stringify(sortKeysRecursive(asset)))
+  //   );
+  //   return oldOwner;
+  // }
 
   // GetAllAssets returns all assets found in the world state.
   @Transaction(false)
